07b5463e0cdba92698a663eb60cbd1667e55a0a3
Abhishek <abhishek.karyan16@gmail.com>
2023-09-01 21:19:21 +0530

    Changes the images and name in main.py file

--- main.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-import gui  # Import the GUI module
-from gui import  FileOrganizerGUI
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = FileOrganizerGUI(root)
-    root.title("FileSync")
-    root.mainloop()
